83d08d3d2883271505bcb4115750963ab652fd48
Zeyu Chen <Zeyu.CHEN@oecd.org>
2023-09-06 12:12:07 +0200

    Updates

---
+++ precache-manifest.27535c3295e78de99b7f53a8fda6844b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63db57e23b546d0c9276c310de5bc3d3",
+    "url": "/food-security-project/index.html"
+  },
+  {
+    "revision": "7485cf335cc7dcd2cd00",
+    "url": "/food-security-project/static/css/2.44583bd4.chunk.css"
+  },
+  {
+    "revision": "67bc54f80a8fa06ba94a",
+    "url": "/food-security-project/static/css/main.ad5e82ae.chunk.css"
+  },
+  {
+    "revision": "7485cf335cc7dcd2cd00",
+    "url": "/food-security-project/static/js/2.aa7cc846.chunk.js"
+  },
+  {
+    "revision": "063d5ca5231c718e83dd6cad2103051b",
+    "url": "/food-security-project/static/js/2.aa7cc846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bc54f80a8fa06ba94a",
+    "url": "/food-security-project/static/js/main.1ba233ef.chunk.js"
+  },
+  {
+    "revision": "ed55e7f6c345d0ab2953",
+    "url": "/food-security-project/static/js/runtime-main.c8911e8c.js"
+  },
+  {
+    "revision": "835ddace27afeb870c92b9ce07351bdf",
+    "url": "/food-security-project/static/media/location-dot-solid.835ddace.svg"
+  },
+  {
+    "revision": "ae02891c000eff70f0f82afd5fb8f015",
+    "url": "/food-security-project/static/media/picture-placeholder.ae02891c.png"
+  }
+]);
