64a010fef992f311070437fc67d5c8eea1dd7782
marianajv02 <130661311+marianajv02@users.noreply.github.com>
2023-09-06 10:18:51 +0200

    Update MapView.js

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,6 +1,6 @@
 
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Legend from './Legend';
 import App from '../App';
