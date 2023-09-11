5546a9b525f2a6ab9c6036e11c296a45e0096e06
zeyu-c <137590459+zeyu-c@users.noreply.github.com>
2023-09-11 15:57:17 +0000

    Updates

---
+++ precache-manifest.dcc66a811a35725605a0fa4f41d86d3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c982adae646f74db784e3016448f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7af70471b569ae7ed9",
+    "url": "/static/css/2.44583bd4.chunk.css"
+  },
+  {
+    "revision": "f0e10b967c44cc455158",
+    "url": "/static/css/main.714cb4ee.chunk.css"
+  },
+  {
+    "revision": "8e7af70471b569ae7ed9",
+    "url": "/static/js/2.13cc2ff9.chunk.js"
+  },
+  {
+    "revision": "063d5ca5231c718e83dd6cad2103051b",
+    "url": "/static/js/2.13cc2ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e10b967c44cc455158",
+    "url": "/static/js/main.0f3c9236.chunk.js"
+  },
+  {
+    "revision": "59ec451622b5f766f8a4",
+    "url": "/static/js/runtime-main.ac1bdf5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/food-security-project/precache-manifest.813f8ded4ac6f104bedc0e14fe5da31f.js"
+  "/precache-manifest.dcc66a811a35725605a0fa4f41d86d3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/food-security-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
