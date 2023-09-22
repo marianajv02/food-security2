94f0c9ee01dcdaf3dcf6cdcf85b6cde428f5ca21
zeyu-c <137590459+zeyu-c@users.noreply.github.com>
2023-09-22 10:22:30 +0200

    Backup build files

---
+++ build/precache-manifest.ed97d8173516771224397cee26a401d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60179c4b2fbf126e9d535997f6ee21a2",
+    "url": "/food-security-project/index.html"
+  },
+  {
+    "revision": "d53cfd64f49cd5f7d6dc",
+    "url": "/food-security-project/static/css/2.44583bd4.chunk.css"
+  },
+  {
+    "revision": "593b14fbbfbccd0efad7",
+    "url": "/food-security-project/static/css/main.a352e699.chunk.css"
+  },
+  {
+    "revision": "d53cfd64f49cd5f7d6dc",
+    "url": "/food-security-project/static/js/2.8e48d8da.chunk.js"
+  },
+  {
+    "revision": "0061bab752f6b8d016ff7121263477cb",
+    "url": "/food-security-project/static/js/2.8e48d8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593b14fbbfbccd0efad7",
+    "url": "/food-security-project/static/js/main.5016d767.chunk.js"
+  },
+  {
+    "revision": "ed55e7f6c345d0ab2953",
+    "url": "/food-security-project/static/js/runtime-main.c8911e8c.js"
+  },
+  {
+    "revision": "835ddace27afeb870c92b9ce07351bdf",
+    "url": "/food-security-project/static/media/location-dot-solid.835ddace.svg"
+  },
+  {
+    "revision": "ae02891c000eff70f0f82afd5fb8f015",
+    "url": "/food-security-project/static/media/picture-placeholder.ae02891c.png"
+  }
+]);
